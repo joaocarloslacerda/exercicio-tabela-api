ad6b7c5d840af8f7a4ceb3630f30300766d1f902
joão <juniorlacerdasilva@gmail.com>
2024-05-11 10:05:53 -0300

    Finalizada funcionalidade de cadastro de novo livro
    adicionada mensagem de sucesso e erro

--- index.js
+++ index.js
@@ -10,5 +10,8 @@ document.getElementById("buttonCadastrar")
 
 
 // 2 - Criar função de início do processo
+    // ->criada função executaCadastro() localizada no arquivo tela.js
 // 3 - Criar função para envio de dados para a API
+    // ->criada função para GET e POST localizadas no arquivo requisicoes.js
 // 4 - Informar tela sobre sucesso de cadastro
+    // ->criada função validaTitulo() localizada no arquivo validaCampos.js
--- validaCampos.js
+++ validaCampos.js
@@ -5,11 +5,13 @@ export function validaTitulo(tituloLivro){
     if(tituloLivro.trim() === ''){
         campoResultado.style.display = "block";
         campoResultado.innerText = "Título obrigatório!";
+        campoResultado.classList.remove("sucesso");
         campoResultado.classList.add("erro");    
     }
     else{
         campoResultado.style.display = "block";
         campoResultado.innerText = "SUCESSO!";
+        campoResultado.classList.remove("erro");
         campoResultado.classList.add("sucesso");
     }
 }
